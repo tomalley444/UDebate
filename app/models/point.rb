90c3814d8237e8825a73c46fd8ff3730c4bf2ed9
Tom <tomalley444@gmail.com>
2016-08-01 23:29:36 +0000

    remove validations

--- app/models/point.rb
+++ app/models/point.rb
@@ -2,6 +2,6 @@ class Point < ActiveRecord::Base
   belongs_to :pointable, polymorphic: true
   has_many :points, as: :pointable
   
-  validates_with SideValidator
+  #validates_with SideValidator
   
 end
